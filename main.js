534c7e2766ff9a860532d181a6e1d9bc293b3281
Jenny Weijland <jennyweij@outlook.com>
2022-11-14 15:22:46 +0100

    lagt till videor

--- main.js
+++ main.js
@@ -3,11 +3,19 @@
     alert(input);
 }*/
 
+let userName = "";
 
 function startButton() {
-    let userWelcome = prompt ("Hej! Vad roligt att du vill spela! Vad heter du?");
-    let userChoice = prompt ("Välkommen! Vill du börja med att lära dig om planeten eller djuren?");
+    let userChoice = confirm ("Välkommen! Här kommer du att få lära dig om djuren och planeten. Vill du börja lära dig om planeten?");
+    if (confirm("Då börjar vi med planeten!") == true) {
+      } 
+      else if (confirm("Då börjar vi med djuren!") == false) {
+      } 
+    let userName = prompt ("Vad heter du?");
+    alert ("Välkommen " + userName + "! Nu börjar vi!") 
+
     //alert("Välkommen " + personChoice , "! Vill du börja med att lära dig om planeten eller djuren?") ;
 
 }
+
 
