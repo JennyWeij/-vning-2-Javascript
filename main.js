e2e4b2266dc601c85cf688aa9a4fae2380bb6359
Jenny Weijland <jennyweij@outlook.com>
2022-11-14 11:35:55 +0100

    uppdaterat knappen på startsidan

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
-function startButton(){
-    let person = prompt ("Vill du lära dig mer om planeten eller djuren först?");
-    if 
+/*function userName() {
+    let name = document.getElementById("userName");
+    alert(input);
+}*/
+
+
+function startButton() {
+    let userWelcome = prompt ("Hej! Vad roligt att du vill spela! Vad heter du?");
+    let userChoice = prompt ("Välkommen! Vill du börja med att lära dig om planeten eller djuren?");
+    //alert("Välkommen " + personChoice , "! Vill du börja med att lära dig om planeten eller djuren?") ;
+
 }
 
