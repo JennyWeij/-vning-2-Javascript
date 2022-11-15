c70e83a594b35bb9b51dc7dd3e1f25c13b288ee3
Jenny Weijland <jennyweij@outlook.com>
2022-11-15 19:08:18 +0100

    uppdaterat inputfält

--- main.js
+++ main.js
@@ -12,8 +12,16 @@ let textStartsida = document.getElementById("textStartsida");
 let button1 = document.getElementById("opt-1");
 let startButton = document.getElementById("startButton");
 
-rubrikStart.textContent = "Välkommen till Planeten!";
+rubrikStart.textContent = "Välkommen till PlanetEn!";
 textStartsida.textContent = "Här får du lära dig mer om vår fantastiska planet och några spännande djur. Fyll i ditt namn och klicka på knappen så kör vi igång!";
 button1.textContent = "Nu kör vi!"
+button1.addEventListener("click", loadPlanetOrAnimals);
 
 }
+
+function loadPlanetOrAnimals() {
+    
+}
+
+
+
